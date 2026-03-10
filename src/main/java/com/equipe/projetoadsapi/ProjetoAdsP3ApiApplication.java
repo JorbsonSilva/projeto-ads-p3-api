13a7c74878ed9d3dddc9b5de8f262d63948381c1
JorbsonSilva <jorbsonsilva93@gmail.com>
2026-03-10 11:31:23 -0300

    Estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/equipe/projetoadsapi/ProjetoAdsP3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.equipe.projetoadsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAdsP3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAdsP3ApiApplication.class, args);
+	}
+
+}
