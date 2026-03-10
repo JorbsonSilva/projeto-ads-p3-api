fe84145d4c1c82c5592a5600366c5fe97f0faa4b
JorbsonSilva <jorbsonsilva93@gmail.com>
2026-03-10 13:38:18 -0300

    Sucesso: Conexão com Supabase estabelecida e tabela Usuario criada

---
+++ src/main/java/com/equipe/projetoadsapi/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.equipe.projetoadsapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USUARIO")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
